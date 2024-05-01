d81fa7a4a3c2db706421803e2896e02297e170cb
GeanCarlosBrandao <ctl_gbrandao@uolinc.com>
2024-05-01 16:01:44 -0300

    Add new screen Bill Details

--- app/src/main/java/com/geanbrandao/br/billbuddy/presentation/billdetails/BillDetailsScreen.kt
+++ app/src/main/java/com/geanbrandao/br/billbuddy/presentation/billdetails/BillDetailsScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
@@ -17,10 +16,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -92,23 +89,23 @@ private fun BillDetailsView(
     }
 }
 
-@Composable
-private fun LazyListState.isScrollingUp(): Boolean {
-    var previousIndex by remember(this) { mutableIntStateOf(firstVisibleItemIndex) }
-    var previousScrollOffset by remember(this) { mutableIntStateOf(firstVisibleItemScrollOffset) }
-    return remember(this) {
-        derivedStateOf {
-            if (previousIndex != firstVisibleItemIndex) {
-                previousIndex > firstVisibleItemIndex
-            } else {
-                previousScrollOffset >= firstVisibleItemScrollOffset
-            }.also {
-                previousIndex = firstVisibleItemIndex
-                previousScrollOffset = firstVisibleItemScrollOffset
-            }
-        }
-    }.value
-}
+//@Composable
+//private fun LazyListState.isScrollingUp(): Boolean {
+//    var previousIndex by remember(this) { mutableIntStateOf(firstVisibleItemIndex) }
+//    var previousScrollOffset by remember(this) { mutableIntStateOf(firstVisibleItemScrollOffset) }
+//    return remember(this) {
+//        derivedStateOf {
+//            if (previousIndex != firstVisibleItemIndex) {
+//                previousIndex > firstVisibleItemIndex
+//            } else {
+//                previousScrollOffset >= firstVisibleItemScrollOffset
+//            }.also {
+//                previousIndex = firstVisibleItemIndex
+//                previousScrollOffset = firstVisibleItemScrollOffset
+//            }
+//        }
+//    }.value
+//}
 
 //@Composable
 //fun LazyListState.isScrollingUp(): State<Boolean> {
