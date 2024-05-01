649992d63732726336a54e55fba12ac6a8aed0cb
GeanCarlosBrandao <ctl_gbrandao@uolinc.com>
2024-05-01 16:02:20 -0300

    Add temp navigation to visualize all the screens

--- app/src/main/java/com/geanbrandao/br/billbuddy/MainActivity.kt
+++ app/src/main/java/com/geanbrandao/br/billbuddy/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -17,7 +21,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.geanbrandao.br.billbuddy.presentation.common.TopAppBarView
+import com.geanbrandao.br.billbuddy.presentation.common.CustomTopAppBar
 import com.geanbrandao.br.billbuddy.presentation.navigation.NavigationView
 import com.geanbrandao.br.billbuddy.presentation.navigation.Screen
 import com.geanbrandao.br.billbuddy.ui.theme.BillBuddyTheme
@@ -31,27 +35,52 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             val canNavigateBack = remember { mutableStateOf(false) }
             val screenTitle = remember { mutableIntStateOf(Screen.Bills.title) }
+            val isTopAppBarVisible = remember { mutableStateOf(true) }
             onDestinationHandle(
-                navController,
-                updateNavigateBack = {canNavigateBack.value = it },
+                navController = navController,
+                updateNavigateBack = { canNavigateBack.value = it },
                 updateScreenTitle = { screenTitle.intValue = it },
+                updateTopAppBarVisible = { isTopAppBarVisible.value = it }
             )
             // TODO temporário
             BillBuddyTheme(darkTheme = false) {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        TopAppBarView(
-                            title = stringResource(id = screenTitle.intValue),
-                            canNavigateBack = canNavigateBack.value,
-                            onArrowBackClicked = { navController.popBackStack() }
-                        )
+                    topBar = if (isTopAppBarVisible.value) {
+                        {
+                            CustomTopAppBar(
+                                title = stringResource(id = screenTitle.intValue),
+                                canNavigateBack = canNavigateBack.value,
+                                onArrowBackClicked = { navController.popBackStack() }
+                            )
+                        }
+                    } else {
+                        { }
                     }
                 ) { innerPadding ->
-                    NavigationView(
-                        modifier = Modifier.padding(innerPadding),
-                        navController = navController,
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Row {
+                            Button(onClick = { navController.navigate(Screen.Bills.route) }) {
+                                Text(text = "1")
+                            }
+                            Button(onClick = { navController.navigate(Screen.Bill.route) }) {
+                                Text(text = "2")
+                            }
+                            Button(onClick = { navController.navigate(Screen.BillDetails.route) }) {
+                                Text(text = "3")
+                            }
+                            Button(onClick = { navController.navigate(Screen.CreateItem.route) }) {
+                                Text(text = "4")
+                            }
+                            Button(onClick = { navController.navigate(Screen.CloseBill.route) }) {
+                                Text(text = "5")
+                            }
+                        }
+                        NavigationView(
+//                            modifier = Modifier.padding(innerPadding),
+                            navController = navController,
+                        )
+                    }
                 }
             }
         }
@@ -62,29 +91,40 @@ private fun onDestinationHandle(
     navController: NavHostController,
     updateNavigateBack: (Boolean) -> Unit,
     updateScreenTitle: (Int) -> Unit,
+    updateTopAppBarVisible: (Boolean) -> Unit,
 ) {
     navController.addOnDestinationChangedListener { _, destination, _ ->
         when (destination.route) {
             Screen.Bills.route -> {
                 updateNavigateBack(false)
+                updateTopAppBarVisible(true)
                 updateScreenTitle(Screen.Bills.title)
             }
+
             Screen.Bill.route -> {
                 updateNavigateBack(true)
+                updateTopAppBarVisible(true)
                 updateScreenTitle(Screen.Bill.title)
             }
+
             Screen.BillDetails.route -> {
-                updateNavigateBack(true)
+                updateNavigateBack(false)
+                updateTopAppBarVisible(false)
                 updateScreenTitle(Screen.BillDetails.title)
             }
+
             Screen.CreateItem.route -> {
                 updateNavigateBack(true)
+                updateTopAppBarVisible(true)
                 updateScreenTitle(Screen.CreateItem.title)
             }
+
             Screen.CloseBill.route -> {
                 updateNavigateBack(true)
+                updateTopAppBarVisible(true)
                 updateScreenTitle(Screen.CloseBill.title)
             }
+
             else -> throw Exception("Something went wrong")
         }
     }
