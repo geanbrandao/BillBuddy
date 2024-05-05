3fb4958d5dcf340eef15b331925a2495345f2238
GeanCarlosBrandao <ctl_gbrandao@uolinc.com>
2024-05-05 17:14:01 -0300

    Add parcelize plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
