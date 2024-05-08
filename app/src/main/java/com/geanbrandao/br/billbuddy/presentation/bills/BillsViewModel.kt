3c4df7af7fda4f2f1040db073a1eb436ed6148aa
GeanCarlosBrandao <ctl_gbrandao@uolinc.com>
2024-05-08 08:02:39 -0300

    Add alguns registros

--- app/src/main/java/com/geanbrandao/br/billbuddy/presentation/bills/BillsViewModel.kt
+++ app/src/main/java/com/geanbrandao/br/billbuddy/presentation/bills/BillsViewModel.kt
@@ -2,6 +2,9 @@ package com.geanbrandao.br.billbuddy.presentation.bills
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.geanbrandao.br.billbuddy.data.local.dao.AppDao
+import com.geanbrandao.br.billbuddy.data.local.entity.BillEntity
+import com.geanbrandao.br.billbuddy.data.local.entity.UserEntity
 import com.geanbrandao.br.billbuddy.presentation.navigation.AppNavigator
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
@@ -9,9 +12,18 @@ import org.koin.android.annotation.KoinViewModel
 @KoinViewModel
 class BillsViewModel(
     private val appNavigator: AppNavigator,
+    private val appDao: AppDao,
 ) : ViewModel() {
 
-
+    init {
+        viewModelScope.launch {
+            val result = appDao.getBills()
+            if (result.isEmpty()) {
+                val id = appDao.insertBill(BillEntity(id = 0, name = "Schema Version 1"))
+                appDao.insertUser(UserEntity(id = 0, name = "User 1", billId = id.toInt()))
+            }
+        }
+    }
 
     fun handleNavigation(intent: BillsNavigationIntent) {
         viewModelScope.launch {
